5246de37b59e9b6724cf397dffc85146e99d48d0
Andrew Gogol <andrewgogol777@gmail.com>
2024-11-05 15:55:27 +0300

    Update with UIButton.Configuration and action, update stack constraints

--- ThreeButtons/ViewController.swift
+++ ThreeButtons/ViewController.swift
@@ -10,50 +10,82 @@ import UIKit
 class ViewController: UIViewController {
 
     let textForButtons = ["First Button", "Second Medium Button", "Third"]
-    lazy var stackView = UIStackView()
+    
+    lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 10
+        view.alignment = .center
+        view.distribution = .fill
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let sideOffset = 16
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .cyan
         view.addSubview(stackView)
         
-        setupStackView()
-        configureButtons()
-        
+        setupStackViewConstraints()
+        setupButtons()
     }
     
-    private func setupStackView() {
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.alignment = .center
-        stackView.distribution = .fill
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        
-        
+    private func setupStackViewConstraints() {
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: CGFloat(self.sideOffset)).isActive = true
+        stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -CGFloat(self.sideOffset)).isActive = true
     }
     
-    private func configureButtons() {
-        for textForButton in textForButtons {
-            let button = UIButton()
-            button.setTitle(textForButton, for: .normal)
+    private func setupButtons() {
+        for (index, textForButton) in textForButtons.enumerated() {
+            let button: UIButton = { configureButton(title: textForButton, needAction: index == 2) }()
+            
             button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
             button.backgroundColor = .systemBlue
             button.layer.cornerRadius = 20
             button.setTitleColor(.white, for: .normal)
             
-            NSLayoutConstraint.activate([
-                button.heightAnchor.constraint(equalToConstant: 50)
-            ])
-            
             stackView.addArrangedSubview(button)
         }
     }
+    
+    private func configureButton(title: String, needAction: Bool) -> ScaleButton {
+        let scaleButton = ScaleButton(configuration: .filled(title: title))
+        
+        if needAction {
+            scaleButton.addAction(UIAction(handler: { [weak self] _ in
+                self?.openModalController()
+            }), for: .touchUpInside)
+        }
+        return scaleButton
+    }
+    
+    private func openModalController() {
+        let controller = UIViewController()
+        controller.view.backgroundColor = .white
+        present(controller, animated: true)
+    }
+}
 
+private extension UIButton.Configuration {
+    
+    static func filled(title title: String) -> Self {
+        var config: UIButton.Configuration = filled()
+        
+        config.title = title
+        config.image = UIImage(systemName: "arrow.right.circle.fill")
+        config.imagePadding = 9
+        config.imagePlacement = .trailing
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 14, bottom: 10, trailing: 14)
+        
+        return config
+    }
+}
+
+class ScaleButton: UIButton {
+    
 }
 
