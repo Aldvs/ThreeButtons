32c6c816058ce4fc5544b4efe31bbd45978353fb
Andrew Gogol <andrewgogol777@gmail.com>
2024-11-05 13:09:39 +0300

    Update ViewController with three buttons in stack

--- ThreeButtons/ViewController.swift
+++ ThreeButtons/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let textForButtons = ["First Button", "Second Medium Button", "Third"]
+    lazy var stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(stackView)
+        
+        setupStackView()
+        configureButtons()
+        
     }
-
+    
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        
+    }
+    
+    private func configureButtons() {
+        for textForButton in textForButtons {
+            let button = UIButton()
+            button.setTitle(textForButton, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+            button.backgroundColor = .systemBlue
+            button.layer.cornerRadius = 20
+            button.setTitleColor(.white, for: .normal)
+            
+            NSLayoutConstraint.activate([
+                button.heightAnchor.constraint(equalToConstant: 50)
+            ])
+            
+            stackView.addArrangedSubview(button)
+        }
+    }
 
 }
 
